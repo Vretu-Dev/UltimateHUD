f152940ad5bde81db3f24f697791946ed0ded8b1
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-08-04 20:15:50 +0200

    correct SSSS id

--- UltimateHUD/Plugin.cs
+++ UltimateHUD/Plugin.cs
@@ -12,7 +12,7 @@ namespace UltimateHUD
         public override Version Version => new Version(6, 2, 1);
         public override Version RequiredExiledVersion { get; } = new Version(9, 6, 0);
         public static Plugin Instance { get; private set; }
-        public HeaderSetting SettingsHeader { get; set; } = new HeaderSetting(5554, "Ultimate HUD");
+        public HeaderSetting SettingsHeader { get; set; } = new HeaderSetting(5553, "Ultimate HUD");
 
         public override void OnEnabled()
         {
