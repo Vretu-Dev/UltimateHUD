2633337eed865b69b2c745e796b5bec528ceafea
Vretu-Dev <jakub.spieczny1@gmail.com>
2025-12-20 15:24:22 +0100

    Bump plugin version

--- UltimateHUD/Plugin.cs
+++ UltimateHUD/Plugin.cs
@@ -10,7 +10,7 @@ namespace UltimateHUD
         public override string Name => "UltimateHUD";
         public override string Description => "Customizable HUD for SCP:SL.";
         public override string Author => "Vretu";
-        public override Version Version => new Version(7, 0, 0);
+        public override Version Version => new Version(7, 0, 2);
         public override Version RequiredApiVersion { get; } = new Version(LabApiProperties.CompiledVersion);
         public static Plugin Instance { get; private set; }
         public Translations Translation { get; private set; }
